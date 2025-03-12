05572a36d96ded097cc1b08077916dfb12b41ec8
Sayid Abdur-Rahman <goldensquirrel11@gmail.com>
2025-03-12 13:59:12 +0800

    Add more training options & improve comments

--- train.py
+++ train.py
@@ -2,6 +2,19 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
 
-    model = YOLO("yolo11n.yaml")
+    # model = YOLO("runs/detect/train3/weights/last.pt")  # For resuming a previous training session
+                                                        # OR using a previous pretrained model weights
+    model = YOLO("yolo11n.yaml")    # Load a COCO-pretrained YOLO11n model
 
-    results = model.train(data="config.yaml", epochs=10000)
+    results = model.train(data="config.yaml",
+                        #   name="train3",    # Name of training run
+                        #   exist_ok=True,    # Allows overwriting an existing project/directory
+                          epochs=500,       # Number of training epochs
+                          time=24,            # Maximum training hours (overrides epochs argument)
+                          batch=0.99,       # GPU memory utilization %
+                        #   cache=True,       # Caches dataset images in RAM (need lots of RAM)
+                          single_cls=True,  # Treats datasets as a single class (useful for object presence rather than classification)
+                          resume=True,      # Resumes training from the last saved checkpoint
+                          profile=True,     # Enables profiling of ONNX and TensorRT speeds during training
+                          plots=True        # Generates & saves plots of training & validation metrics as well as prediction examples
+                          )
