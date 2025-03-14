8c731f27cdf677a3fe77665339539612431a7d40
Sayid Abdur-Rahman <goldensquirrel11@gmail.com>
2025-03-15 00:09:16 +0800

    calculate fps from inference speed

--- predict_video.py
+++ predict_video.py
@@ -16,6 +16,38 @@ out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.g
 
 model_path = os.path.join('.', 'runs', 'detect', 'train24', 'weights', 'last.pt')
 
+total_inference_time = 0.0
+frames = 0
+
+# Load a model
+model = YOLO(model_path)  # load a custom model
+
+threshold = 0.5
+
+while ret:
+
+    results = model(frame)[0]
+    total_inference_time += results.speed['preprocess']
+    total_inference_time += results.speed['inference']
+    total_inference_time += results.speed['postprocess']
+    frames += 1
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+    out.write(frame)
+    ret, frame = cap.read()
+
+print()
+print("Average Inference Time per frame: ", (total_inference_time/frames))
+print("FPS based on Average Inference Time: ", (1/(total_inference_time/(frames*1000))))
+
+
 # Load a model
 model = YOLO(model_path)  # load a custom model
 
