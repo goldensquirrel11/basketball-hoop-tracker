af88ac48d3b9a06bb774b69d2ae8dab540420962
Sayid Abdur-Rahman Al-Aidarus <goldensquirrel11@gmail.com>
2025-06-09 21:22:36 +0800

    Fix batch_predict_video: smaller font size and class colors

--- batch_predict_video.py
+++ batch_predict_video.py
@@ -92,9 +92,9 @@ def batch_predict(video_dir: Path, output_dir: Path, project_dir: Path):
                         x1, y1, x2, y2, score, class_id = result
 
                         if score > threshold:
-                            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
+                            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0) if class_id == 1 else (0, 0, 255), 1)
                             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 3, cv2.LINE_AA)
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0) if class_id == 1 else (0, 0, 255), 1, cv2.LINE_AA)
                     
                     # Display FPS
                     cv2.putText(frame, str(int(fps)), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
